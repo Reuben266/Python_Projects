32510eebd5b68ec82b0248f56aa4e84d532e479b
Reuben266 <reubenahadzi629@gmail.com>
2026-09-19 19:57:22 +0000

    Implement JSON data persistence and class instance deserialization

--- Classes/bank_acount/bank_account.py
+++ Classes/bank_acount/bank_account.py
@@ -1,8 +1,145 @@
 import bank_account_class as ba
 import json
 
-ac1 = ba.BankAccount("reuben", "0534543355", "reu@gmail.com", 0)
-ac2 = ba.SavingsAcount("bright", "0248547898", "bri@gmail.com", 17, 200)
+account_list = []
+def convert_to_class_instance():
+    try:
+      with open('account.json', 'r') as file:
+        accounts = json.load(file)
+        for account in accounts:
+          if account['account_type'] == "BankAccount":
+            banking = ba.BankAccount(account['name'], account['phone_number'], account['email'], account['balance'])
+            banking._amount_owed = account['amount_owed']
+            banking.transaction = account['transaction history']
+            account_list.append(banking)
+          if account['account_type'] == "Savings Account":
+            saving = ba.SavingsAccount(account['name'], account['phone_number'], account['email'], account['age'], account['balance'])
+            account_list.append(saving)
 
-account = {ac1, ac2}
+        return account_list
+    except FileNotFoundError:
+      accounts = []
+convert_to_class_instance()
 
+user_options = ["[1] Add Bank Account", "[2] Add Savings Account", "[3] Deposit", "[4] Withdraw", "[5] Take loan", "[6] Pay loan", "[7] Save and Exit"]
+
+while True:
+  for option in user_options:
+    print(option)
+
+  try:
+    choice = int(input("What would you like to do:  "))
+  except ValueError:
+    print("Invalid input")
+    continue
+
+  if choice == 1:
+    account_type = "BankAccount"
+    name = input("Enter account name::  ")
+    phone_number = input("What is your phone number::  ")
+    email = input("Enter your email:: ")
+
+    Bank = ba.BankAccount(name, phone_number, email)
+    account_list.append(Bank)
+
+  elif choice == 2:
+    account_type = "BankAccount"
+    name = input("Enter account name::  ")
+    phone_number = input("What is your phone number::  ")
+    email = input("Enter your email:: ")
+    age = int(input("What is you age::  "))
+
+    Save = ba.SavingsAccount(name, phone_number, email, age)
+    account_list.append(Save)
+
+  elif choice == 3:
+    phone = input("Enter your phone number::  ")
+    verification = input("Enter your phone number again::  ")
+
+    if phone != verification:
+      print("Phone number mismatch!")
+      continue
+    else:
+      found = False
+      for account in account_list:
+        if account.phone_number == phone:
+          found = True
+          break
+          
+      if found:
+        amount = int(input("How much do you want to deposit::  "))
+        account.deposit(amount)
+      else:
+        print("Account not found")
+
+  elif choice == 4:
+    phone = input("Enter your phone number::  ")
+    verification = input("Enter your phone number again::  ")
+
+    if phone != verification:
+      print("Phone number mismatch!")
+      continue
+    else:
+      found = False
+      for account in account_list:
+        if account.phone_number == phone:
+          found = True
+          break
+          
+      if found:
+        amount = int(input("How much do you want to Withdraw::  "))
+        account.withdraw(amount)
+      else:
+        print("Account not found")
+
+  elif choice == 5:
+    phone = input("Enter your phone number::  ")
+    verification = input("Enter your phone number again::  ")
+
+    if phone != verification:
+      print("Phone number mismatch!")
+      continue
+    else:
+      found = False
+      for account in account_list:
+        if account.phone_number == phone:
+          found = True
+          break
+          
+      if found:
+        amount = int(input("Enter loan amount::  "))
+        duration = int(input("Enter the duration of the loan"))
+        account.loan(amount, duration)
+      else:
+        print("Account not found")
+
+  elif choice == 6:
+    phone = input("Enter your phone number::  ")
+    verification = input("Enter your phone number again::  ")
+
+    if phone != verification:
+      print("Phone number mismatch!")
+      continue
+    else:
+      found = False
+      for account in account_list:
+        if account.phone_number == phone:
+          found = True
+          break
+          
+      if found:
+        amount = int(input("Enter amount::  "))
+        account.pay_loan(amount)
+      else:
+        print("Account not found")
+
+  elif choice == 7:
+    saved_account = []
+    for account in account_list:
+      finished = account.convert_to_class_instance_to_dic()
+      saved_account.append(finished)
+
+    with open('account.json', 'w') as file:
+      json.dump(saved_account, file, indent = 5)
+
+    break
--- Classes/bank_acount/bank_account_class.py
+++ Classes/bank_acount/bank_account_class.py
@@ -1,3 +1,5 @@
+import json
+
 class BankAccount:
   def __init__(self, name, phone_number, email, balance=0):
     self.name = str(name)
@@ -6,63 +8,82 @@ class BankAccount:
     self._balance = float(balance)
     self._amount_owed = 0
     self.transaction = []
-    
-    
 
   def deposit(self, amount):
-    if amount < 0:
-      return "Invalid input, the amount must be greater than 0.00"
+    if amount < 0 or amount == 0:
+      result = "Invalid input, the amount must be greater than 0.00"
+      print(result)
     else:
       self._balance += amount
       self.transaction.append(f"Deposited: ${amount:.2f}")
-      return f"Deposited: ${amount:.2f}, current balance: ${self._balance:.2f}"
-
+      result = f"Deposited: ${amount:.2f}, current balance: ${self._balance:.2f}"
+      print(result)
+      
   
   def withdraw(self, amount):
     if amount > self._balance:
       return f"Cannot withdraw an amount greater than your balance. Current balance: ${self._balance:.2f}"
+    
+    elif amount < 0 or amount == 0:
+      result = "Cannot withdraw an amount less than or equal to $0.00"
+      print(result)
+      return result
+    
     else:
-      if amount < 0:
-        return "Cannot withdraw an amount less than $0.00"
-      else:
-        self._balance -= amount
-        self.transaction.append(f"Withdrew ${amount:.2f}")
-        return f"Withdrew ${amount:.2f}, current balance ${self._balance:.2f}"
-  
+      self._balance -= amount
+      self.transaction.append(f"Withdrew ${amount:.2f}")
+      result = f"Withdrew ${amount:.2f}, current balance ${self._balance:.2f}"
+      print(result)
+      return result
   
   def loan(self, amount, time):
     if self._balance == 0:
-      return "you Cannot aquire a loan if your account balance is $0.00"
+      result = "you Cannot acquire a loan if your account balance is $0.00"
+      print(result)
       
     elif time <= 0:
-      return "Invalid input, negative time"
-    
+      result = "Invalid input, negative time"
+      print(result)
+  
+    elif amount < 0 or amount == 0:
+      result = "Invalid input, cannot take a loan less than or equal to $0.00"
+      print(result)
+      
     else:
       SI = (amount * 25 * time)/100
-      self._amount_owed = amount + SI
+      self._amount_owed += (amount + SI)
       self._balance += amount
       self.transaction.append(f"Took a loan of ${amount:.2f} for a time of {time} year(s)")
-      return f"Took a loan of ${amount:.2f}, interest on ${amount:.2f} is ${SI:.2f}, total _amount_owed: ${self._amount_owed:.2f}, current balance: ${self._balance:.2f}"
-
+      result = f"Took a loan of ${amount:.2f}, interest on ${amount:.2f} is ${SI:.2f}, total _amount_owed: ${self._amount_owed:.2f}, current balance: ${self._balance:.2f}"
+      print(result)
   
   def pay_loan(self, amount):
     if self._amount_owed == 0:
-      return f"Loan already paid, current balance: ${self._balance:.2f}"
+      result = f"Loan already paid, current balance: ${self._balance:.2f}"
+      print(result)
+    elif amount > self._balance:
+      result = "Insufficient balance"
+      print(result)
       
     elif amount > self._amount_owed:
+      self.transaction.append(f"Paid ${self._amount_owed:.2f}")
       amount -= self._amount_owed
       self._balance -= self._amount_owed
       change = amount
       self._amount_owed = 0
-      return f"Amount left: ${self._amount_owed:.2f}. Change of ${change:.2f} was returned. Current account balance: ${self._balance}"
+      result = f"Amount left: ${self._amount_owed:.2f}. Change of ${change:.2f} was returned. Current account balance: ${self._balance:.2f}"
+      print(result)
       
     elif amount <= 0:
-      return "Amount must be greater than $0.00"
+      result="Amount must be greater than $0.00"
+      print(result)
       
     else:
       self._amount_owed -= amount
       self._balance -= amount
-      return f"paid an amount of: ${amount:.2f}. total amount left: ${self._amount_owed:.2f}. Current account balance: ${self._balance:.2f}"
+      self.transaction.append(f"Paid ${amount:.2f}")
+      result=f"paid an amount of: ${amount:.2f}. total amount left: ${self._amount_owed:.2f}. Current account balance: ${self._balance:.2f}"
+      print(result)
 
   
   def transfer(self, target_account, amount):
@@ -72,30 +93,40 @@ class BankAccount:
     elif self._balance < amount:
       return f"Cannot send an amount greater than your account balance, current account balance: ${self._balance:.2f}"
       
-    else:
-      if amount <= 0:
-        return "Invalid transfer, cannot send an amount less than or equal to $0.00"
+    
+    elif amount <= 0:
+      return "Invalid transfer, cannot send an amount less than or equal to $0.00"
         
-      else:
-        if target_account.name == self.name:
-          return "Cannot transfer money to yourself"
-          
-        else:
-          self._balance -= amount
-          target_account.deposit(amount)
-          return f"Sent ${amount:.2f} to {target_account.name}"
       
-  def convert_to_class_instance_to_dic(self):
-    ...
+    elif target_account is self:
+      return "Cannot transfer money to yourself"
+          
+    else:
+      self._balance -= amount
+      target_account._balance += amount
+      target_account.transaction.append(f"Received ${amount:.2f} from {self.name}")
+      self.transaction.append(f"Transferred ${amount:.2f} to {target_account.name}")
+      return f"Sent ${amount:.2f} to {target_account.name} current balance ${self._balance:.2f}"
 
-  def convert_to_class_instance(self):
-    ...
+    
+  def convert_to_class_instance_to_dic(self):
+    account = {
+      "account_type": "BankAccount",
+      "name": self.name,
+      "phone_number": self.phone_number,
+      "email": self.email,
+      "balance": self._balance,
+      "amount_owed": self._amount_owed,
+      "transaction history": self.transaction
+    }
+
+    return account
     
   def __str__(self):
-    return f"Acount name: {self.name} | Acount balance: ${self._balance:.2f}"
+    return f"Account name: {self.name} | Account balance: ${self._balance:.2f}"
 
 
-class SavingsAcount(BankAccount):
+class SavingsAccount(BankAccount):
   def __init__(self, name, phone_number, email, age, balance=0.00):
     super().__init__(name, phone_number, email, balance)
     self.age = int(age)
@@ -106,13 +137,21 @@ class SavingsAcount(BankAccount):
     interest = self._balance * 0.05
     self._balance += interest
     new_balance = self._balance
+    self.transaction.append(f"Interest of ${interest:.2f} calculated and added to savings account current balance: ${self._balance:.2f}")
     return f"0.05 interest was calculated on ${old_balance:.2f}. Total balance: ${new_balance:.2f}"
+
+  def convert_to_class_instance_to_dic(self):
+    account = super().convert_to_class_instance_to_dic()
+    account['account_type'] = "SavingsAccount"
+    account['age'] = self.age
+
+    return account
+  
 
   
   def __str__(self):
-    return f"Acount name: {self.name} | Age: {self.age} | Balance: ${self._balance:.2f}"
-      
+    return f"Account name: {self.name} | Age: {self.age} | Balance: ${self._balance:.2f}"
 
-ac1 = BankAccount("Reuben", "08888", "reu@ben.com", 1000)
-ac2 = SavingsAcount("Bright", "066666", "bri@gmail.com", "20", 200)
-print(ac1.loan(200,-6))
+ac1 = BankAccount("Reuben", "00777", "ben.com", 1000)
+print(ac1.loan(200, 2))
+print(ac1._amount_owed)
